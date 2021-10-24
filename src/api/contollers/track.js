f7cacf1fb4b2e1502813f72ed57134efa8a64739
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-10-24 13:20:24 +0530

    Tracks API updated

--- src/api/contollers/track.js
+++ src/api/contollers/track.js
@@ -8,10 +8,27 @@ const TrackController = {
         const tracks = await trackService.getAllTracks();
         res.json(tracks);
     }),
-    // getTrackById: asyncHandler(async (req, res) => {
-    //     const track = await trackService.getTrackById(req.params.id);
-    //     res.json(track);
-    // }),
+    getTrack: asyncHandler(async (req, res) => {
+        const track = await trackService.getTrackById(req.params.id);
+        res.json(track);
+    }),
+    createTrack: asyncHandler(async (req, res) => {
+        req.body.created_by = req.user._id;
+        const track = await trackService.createTrack(req.body);
+        res.json(track);
+    }),
+    deleteAllTracks: asyncHandler(async (req, res) => {
+        await trackService.deleteAllTracks();
+        res.json({
+            message: "All tracks deleted"
+        });
+    }),
+    deleteTrack: asyncHandler(async (req, res) => {
+        await trackService.deleteTrack(req.params.id);
+        res.json({
+            message: "Track deleted"
+        });
+    }),
 }
 
 module.exports = TrackController;
--- src/api/services/track.service.js
+++ src/api/services/track.service.js
@@ -1,30 +1,51 @@
-// const Tracks = require('../models/track');
+const Tracks = require('../models/track');
 
 class TrackService {
     async getAllTracks() {
         try {
-            // allTracks = await Tracks.find({});
-            return "asdf";
+            const allTracks = await Tracks.find({visibility: true});
+            return allTracks;
         } catch (error) {
             console.error(error);
         }
     }
 
-    // getTrack(id) {
-    //     return this.http.get(`/api/tracks/${id}`);
-    // }
+    async getTrackById(id) {
+        try {
+            const track = await Tracks.findById(id);
+            return track;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-    // createTrack(data) {
-    //     return this.http.post('/api/tracks', data);
-    // }
+    async createTrack(data) {
+        try {
+            const track = new Tracks(data);
+            return await track.save();
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-    // updateTrack(id, data) {
-    //     return this.http.put(`/api/tracks/${id}`, data);
-    // }
+    async deleteAllTracks() {
+        try {
+            await Tracks.deleteMany();
+            return 'All tracks deleted';
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-    // deleteTrack(id) {
-    //     return this.http.delete(`/api/tracks/${id}`);
-    // }
+    async deleteTrack(id) {
+        try {
+            console.log(id);
+            await Tracks.findByIdAndDelete(id);
+            return 'Track deleted';
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 module.exports = TrackService;
